a0eda77c1c8ad4b4d7a4e30f63fc219106f9cb53
Simon Charters <simon259@hotmail.co.uk>
2020-11-27 12:45:54 +0000

    initial setup

---
+++ tests/song_test.py
@@ -0,0 +1,7 @@
+import unittest
+from src.song import Song
+
+class TestSong(unittest.TestCase):
+    def setUp(self):
+        never_gonna_give_you_up = Song("Never Gonna Give You Up", "Rick Astley", pop )
+
