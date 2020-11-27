4f09c48215599e63cff96ee1753d203a3432b3c2
Simon Charters <simon259@hotmail.co.uk>
2020-11-27 12:51:06 +0000

    song - name tested

--- tests/song_test.py
+++ tests/song_test.py
@@ -3,5 +3,8 @@ from src.song import Song
 
 class TestSong(unittest.TestCase):
     def setUp(self):
-        never_gonna_give_you_up = Song("Never Gonna Give You Up", "Rick Astley", pop )
+        self.never_gonna_give_you_up = Song("Never Gonna Give You Up", "Rick Astley", "Pop" )
+
+    def test_song_has_name(self):
+        self.assertEqual("Never Gonna Give You Up", self.never_gonna_give_you_up.name)
 
