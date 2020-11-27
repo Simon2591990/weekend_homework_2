6f94d08149ebe5303dfba5ad61b24e6b4b2c97d5
Simon Charters <simon259@hotmail.co.uk>
2020-11-27 13:06:33 +0000

    guest_test setup complete

--- run_tests.py
+++ run_tests.py
@@ -1,5 +1,6 @@
 import unittest
 from tests.song_test import TestSong
+from tests.guest_test import GuestTest
 
 if __name__ == "__main__":
     unittest.main()
---
+++ tests/guest_test.py
@@ -0,0 +1,6 @@
+import unittest
+from src.guest import guest
+
+class TestGuest(unittest.TestCase):
+    def setUp(self):
+        self.guest = ("Singy McSingface", 36)
