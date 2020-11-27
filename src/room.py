22d65fb0191de67d0c3a1e6895664108248468a8
Simon Charters <simon259@hotmail.co.uk>
2020-11-27 13:54:53 +0000

    room - name tested

--- src/room.py
+++ src/room.py
@@ -1,4 +1,6 @@
 class Room:
-    def __init__(self, party, capacity):
-        self.party = party
+    def __init__(self, room_name, capacity):
+        self.guests = []
+        self.songs = []
         self.capacity = capacity
+        self.room_name = room_name
--- tests/room_test.py
+++ tests/room_test.py
@@ -7,10 +7,13 @@ class TestRoom(unittest.TestCase):
         person_1 = Guest("Songy McSongface", 36)
         person_2 = Guest("Singy McSingface", 34)
         party = [person_1, person_2]
-        self.room = Room(party, 5)
+        self.room = Room("Room 1", 5)
 
     def test_room_capacity(self):
         self.assertEqual(5, self.room.capacity)
+    
+    def test_room_name(self):
+        self.assertEqual("Room 1", self.room.room_name)
 
 
     
