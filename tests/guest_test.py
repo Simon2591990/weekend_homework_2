c55cfbaad6baa7b8002c782ca5ee20918658bfce
Simon Charters <simon259@hotmail.co.uk>
2020-11-27 13:09:13 +0000

    guest - name tested

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -1,6 +1,11 @@
 import unittest
-from src.guest import guest
+from src.guest import Guest
 
 class TestGuest(unittest.TestCase):
     def setUp(self):
-        self.guest = ("Singy McSingface", 36)
+        self.guest = Guest("Singy McSingface", 36)
+
+    def test_guest_has_name(self):
+        self.assertEqual("Singy McSingface", self.guest.name)
+
+    
